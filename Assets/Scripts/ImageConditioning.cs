188413c75621e9b1590d62ea07d3f788763ee07f
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-02-08 07:20:01 +0100

    Update ImageConditioning.cs

--- Assets/Scripts/ImageConditioning.cs
+++ Assets/Scripts/ImageConditioning.cs
@@ -7,6 +7,7 @@ public class ImageConditioning : MonoBehaviour
     Texture2D original_map; //
     Texture2D modified_map; //
     GameObject modified_driving_lane;
+    Sprite modified_map_sprite;
 
     void Start()
     {
@@ -15,7 +16,7 @@ public class ImageConditioning : MonoBehaviour
         float heigth_of_images = original_map.height;
         float width_of_images = original_map.width;
         
-
+        //Get the modified driving lane (child object)
         modified_driving_lane = transform.GetChild(1).gameObject;
 
         //Create new texture with properties that I want
@@ -25,11 +26,15 @@ public class ImageConditioning : MonoBehaviour
         modified_map.SetPixels(original_map.GetPixels());
         modified_map.Apply();
 
-        modified_driving_lane.GetComponent<SpriteRenderer>().sprite = Sprite.Create(modified_map, )
+        modified_map_sprite = Sprite.Create(modified_map, new Rect(0, 0, modified_map.width, modified_map.height), new Vector3(0,0,0));
+
+        modified_driving_lane.GetComponent<SpriteRenderer>().sprite = modified_map_sprite;
+
+        Debug.Log(modified_driving_lane.name);
     }
 
     void Update()
-    {
-        
+    { 
+        modified_driving_lane.GetComponent<SpriteRenderer>().sprite = modified_map_sprite;
     }
 }
