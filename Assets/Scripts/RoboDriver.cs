d91f9767ee4f998e6a80a2db066ee90bc82b68da
AGPFVEN <alfonso_pineda2003@hotmail.com>
2022-02-09 07:20:11 +0100

    Update RoboDriver.cs

--- Assets/Scripts/RoboDriver.cs
+++ Assets/Scripts/RoboDriver.cs
@@ -11,13 +11,39 @@ public class RoboDriver : MonoBehaviour
     //Movement variables
     public float thrust = 500f;
     public float friction = 0.993f;
-    Vector3 actual_direction = new Vector3(0, 0, 0);
+    Vector3 actual_direction = new Vector3(0, 0, 0); 
+    Texture2D original_map; //
+    Texture2D modified_map; //
+    GameObject modified_driving_lane;
+    Sprite modified_map_sprite;
+
 
     // Start is called before the first frame update
     void Start()
     {
         //Components
         rb = gameObject.GetComponent<Rigidbody>();
+
+        //Get the Sprite of the driving line (from the gameobject, which is a child object)
+        original_map = GameObject.Find("Image Manager").transform.GetChild(0).GetComponent<SpriteRenderer>().sprite.texture;
+        float heigth_of_images = original_map.height;
+        float width_of_images = original_map.width;
+        
+        //Get the modified driving lane (child object)
+        modified_driving_lane = GameObject.Find("Image Manager").transform.GetChild(1).gameObject;
+
+        //Create new texture with properties that I want
+        modified_map = new Texture2D(original_map.width, original_map.height, TextureFormat.RGBA32, false);
+
+        //Copy texture from the original to the modified one
+        modified_map.SetPixels(original_map.GetPixels());
+        modified_map.Apply();
+
+        modified_map_sprite = Sprite.Create(modified_map, new Rect(0, 0, modified_map.width, modified_map.height), new Vector3(0,0,0));
+
+        modified_driving_lane.GetComponent<SpriteRenderer>().sprite = modified_map_sprite;
+
+        Debug.Log(modified_driving_lane.name);
     }
 
     // Update is called once per frame
@@ -38,7 +64,7 @@ public class RoboDriver : MonoBehaviour
 
         #region Raycasts
 
-            Vector3 right_vector = this.transform.forward;
+            Vector3 right_vector = this.transform.up;
             Vector3 left_vector = this.transform.up - this.transform.right;
 
             Debug.DrawRay(transform.position, right_vector * 10, Color.green);
@@ -57,11 +83,17 @@ public class RoboDriver : MonoBehaviour
                     (int)right_raycast.point.y - (int)map_gameobject.transform.position.y + (int)(map.height / 2)
                 ));
 
-                map.SetPixel(
-                    (int)right_raycast.point.x - (int)map_gameobject.transform.position.x + (int)(map.width / 2),
-                    (int)right_raycast.point.y - (int)map_gameobject.transform.position.y + (int)(map.height / 2),
-                    Color.cyan
-                );
+                Texture2D mod_ = modified_driving_lane.GetComponent<SpriteRenderer>().sprite.texture;
+
+                int x_ = (int)right_raycast.point.x - (int)map_gameobject.transform.position.x + (int)(map.width / 2);
+                int y_ = (int)right_raycast.point.y - (int)map_gameobject.transform.position.y + (int)(map.height / 2);
+                
+                mod_.SetPixel(x_, y_, Color.black);
+
+                mod_.Apply();
+
+                Debug.Log("heigth: " + (mod_.height).ToString() + "     width: " + mod_.width.ToString());
+                Debug.Log("x: " + x_.ToString() + "      y: " + y_.ToString());
             }
             //if (Physics.Raycast(transform.position, left_vector, out left_raycast))
             //{
